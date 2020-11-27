7ff6a81a1baf4c21ada99a833ef3294a344c37a6
ManuWer <manoelawerneck@gmail.com>
2020-11-27 00:35:12 -0300

    file transfer

--- client.py
+++ client.py
@@ -7,32 +7,62 @@ PORT = 20000
 udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 # udp.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 serv = (HOST, PORT)
-# udp.bind(serv)
 
 connected = True # bool de controle
 
+def receiveFile(con, name):
+    f = open('./clientFiles/' + name,'wb')
+    while True:
+        data = con.recv(1024)
+        if not data:
+            break
+        f.write(data)
+    f.close()
+
+    return
+
+def sendFile(con, name):
+    f = open('./clientFiles/' + name,'rb')
+    for msg in f.readlines():
+        con.send(msg)
+    f.close()
+
+    return
+
 # funcao que codifica o comando nas palavras-chave do protocolo
 def send():
-
     while True:
         msg = input().split(" ")
+
         if msg[0] == '/list':
             text = "LIST"
+            udp.sendto(text.encode(), serv)
+
         elif msg[0] == '/file':
+            tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            tcp.connect(serv)
             text = "FILE:" + msg[1]
-            # criar conexao TCP...
+            udp.sendto(text.encode(), serv)
+            sendFile(tcp, msg[1])
+            tcp.close()
+
         elif msg[0] == '/get':
+            tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            tcp.connect(serv)
             text = "GET:" + msg[1]
-            # criar conexao TCP...
+            udp.sendto(text.encode(), serv)
+            receiveFile(tcp, msg[1])
+            tcp.close()
+
         elif msg[0] == '/bye':
             text = "BYE"
             udp.sendto(text.encode(), serv)
             connected = False
             break
+
         else:
             text = "MSG:" + " ".join(msg)
-
-        udp.sendto(text.encode(), serv)
+            udp.sendto(text.encode(), serv)
 
     udp.close()
     return
@@ -42,7 +72,12 @@ def listen():
         msg, client = udp.recvfrom(1024)
         msg = msg.decode().split(":")
         if msg[0] == 'INFO':
-            print(msg[1])
+            if msg[1] == 'disconnect':
+                break
+            elif len(msg) < 3:
+                print(msg[1])
+            else:
+                print(msg[1] + ': ' + msg[2])
         elif msg[0] == 'MSG':
             print(msg[1] + ': ' + msg[2])
 
@@ -57,9 +92,3 @@ t1.start()
 
 t2 = threading.Thread(target=listen, args=())
 t2.start()
-
-# while msg != '/bye':
-#     udp.sendto(msg.encode(), (HOST, PORT))
-#     msg, serv = udp.recvfrom(1024)
-#     print(serv, msg.decode())
-#     msg = input()
--- server.py
+++ server.py
@@ -5,7 +5,7 @@ HOST = ''
 PORT = 20000
 
 udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-udp.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+#udp.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 udp.bind((HOST, PORT))
 
 clients = {}    # guarda tupla (IP, porta) e nome de clientes ativos no momento
@@ -15,57 +15,76 @@ def forwardMessage(msg, client):
         if client_socket != client: # exceto para o cliente que enviou a msg
             udp.sendto(msg.encode(), client_socket) # envia msg
 
+def receiveFile(con, name):
+    f = open(name,'wb')
+    while True:
+        data = con.recv(1024)
+        if not data:
+            break
+        f.write(data)
+    f.close()
+
+    return
+
+def sendFile(con, name):
+    f = open(name,'rb')
+    for msg in f.readlines():
+        con.send(msg)
+    f.close()
+
+    return
 
 def receive():
-
     while True:
+        tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        tcp.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        tcp.bind((HOST, PORT))
+        tcp.listen(1)
+
         msg, client = udp.recvfrom(1024)
         msg = msg.decode().split(":")
-        print(msg)
+
         if msg[0] == 'USER':
             clients[client] = msg[1] # adiciona em clientes conectados
             text = 'INFO:' + msg[1] + ' entrou'
             forwardMessage(text, client)
+
         elif msg[0] == 'MSG':
             text = "MSG:" + clients[client] + ":" + msg[1]
             forwardMessage(text, client)
+
         elif msg[0] == 'LIST':
-            print("Encaminhando lista...")
-            text = 'Online:\n'
+            print("Enviando lista de clientes ativos.")
+            text = 'INFO:Online:\n'
             for client_socket in clients: # todos os clientes conectados
                 text += clients[client_socket] + ", "
             udp.sendto(text.encode(), client) # envia apenas para quem pediu a lista
+
         elif msg[0] == 'FILE':
-            # ...
-            text = 'INFO:' + clients[client] + ' enviou '
+            con, clientTCP = tcp.accept()
+            receiveFile(con, msg[1])
+            print("Arquivo recebido.")
+            text = 'INFO:' + clients[client] + ' enviou ' + msg[1]
             forwardMessage(text, client)
+            con.close()
+
         elif msg[0] == 'GET':
-            #...
-            print('get')
+            con, clientTCP = tcp.accept()
+            sendFile(con, msg[1])
+            print('Arquivo enviado.')
+            con.close()
+
         elif msg[0] == 'BYE':
             text = 'INFO:' + clients[client] + ' saiu'
             forwardMessage(text, client)
             del clients[client] # remove de clientes conectados
+            udp.sendto('INFO:disconnect'.encode(), client) # envia msg de controle para quem pediu pra sair para de ouvir o servidor
 
+    tcp.close()
     return
 
-t = threading.Thread(target=receive, args=())
-t.start()
-
-# udp.close()
-
-# while True:
-#     print('esperando...')
-#     msg, client = udp.recvfrom(1024)
-#     print(client, msg.decode())
-#     udp.sendto('ack'.encode(), client)
-
-# tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# tcp.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-# tcp.bind((HOST, PORT))
-
-# tcp.listen(1)
+t1 = threading.Thread(target=receive, args=())
+t1.start()
 
 
 
